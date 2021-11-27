a267097e07fd8e0d49f5b4808369f6e1380f080d
Cristobal Dominguez <cristobald@gmail.com>
2021-11-27 17:51:57 -0300

    Improves db Connection

--- db/index.js
+++
@@ -1,4 +0,0 @@
-import { Pool } from 'pg'
-import { db as pg_connection } from '../config'
-
-const pool = new Pool(pg_connection)
---
+++ db/pool.js
@@ -0,0 +1,7 @@
+import pg from 'pg';
+import { db as pg_connection } from '../config.js'
+
+const { Pool } = pg
+const pool = new Pool(pg_connection)
+
+export default pool
