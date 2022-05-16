a8ddf6dda5e7387b1ff4e6770328c3614644ecb2
Cristobal Dominguez <cristobald@gmail.com>
2022-05-16 17:34:23 +0200

    Small Improvements
    Adds Improvements on .env and Config Files
    
    Adds Small Improvements

--- config.js
+++ config.js
@@ -1,14 +1,23 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import dotenv from 'dotenv'
+
+// dotEnv Config
+dotenv.config()
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const env_db_config = JSON.parse(process.env.DB_CONFIG)
 
 const port = process.env.PORT || 3000
 const root = dirname(fileURLToPath(import.meta.url))
-const db = {
-    host: 'localhost',
-    user: 'pg-username',
-    password: 'pg-password',
-    database: 'db-name',
-    port: 5432
+const db_default_options = {
+    host: process.env.DB_HOST || 'localhost',
+    // user: 'pg_username',
+    // password: 'pg_password',
+    // database: 'db_name',
+    port: Number(process.env.DB_PORT) || 5432
 }
+
+const db = { ...db_default_options, ...env_db_config }
 
 export { port, root, db }
--- routes/main.js
+++ routes/main.js
@@ -3,9 +3,9 @@ import express from 'express'
 // Router Creation
 const router = express.Router()
 
-import { home_main } from '../controllers/main_controller.js'
+import { home_main_controller } from '../controllers/main_controller.js'
 
 // Routes
-router.get('/', home_main)
+router.get( '/', home_main_controller )
 
 export default router
